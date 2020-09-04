6bc1373a5a831ec1f259c3ee5bd9bae770d6024d
shameekagarwal <shameek.agarwal@gmail.com>
2020-09-04 16:19:08 +0530

    test programming assignment

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include<stdio.h>
-int main()
-{
-  printf("Hello");
-  //Add your code below
+#include <pthread.h>
+int sum;
+void runner(void param);
+int main(int argc, char* argv[]) {
+  pthread_t tid;
+  pthread_attr_t attr;
+  if (argc != 2) {
+    fprintf(stderr, "usage: a.out <integer value>\n");
+    return -1;
+  }
+  if (atoi(argv[1]) < 0) {
+    fprintf(stderr, "%d must be >= 0\n", atoi(argv[1]));
+    return -1;
+  }
+  printf("Sum = %d\n", sum);
+}
+void runner(void param) {
+  int i, upper = atoi(param);
+  sum = 0;
+  for (i = 1; i <= upper; i++) {
+    sum += i;
+  }
+  pthread_t.exit(0);//this line shows error i dont know
 }
