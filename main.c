dd33532fc16c46e5305895b6c3d6564965ac9ecf
shameekagarwal <shameek.agarwal@gmail.com>
2020-09-04 16:33:12 +0530

    corrected issues

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include <pthread.h>
+#include<stdlib.h>
 int sum;
-void runner(void param);
+void runner(char * param);
 int main(int argc, char* argv[]) {
   pthread_t tid;
   pthread_attr_t attr;
@@ -15,11 +16,11 @@ int main(int argc, char* argv[]) {
   }
   printf("Sum = %d\n", sum);
 }
-void runner(void param) {
+void runner(char *param) {
   int i, upper = atoi(param);
   sum = 0;
   for (i = 1; i <= upper; i++) {
     sum += i;
   }
-  pthread_t.exit(0);//this line shows error i dont know
+  pthread_exit(0);//this line shows error i dont know
 }
